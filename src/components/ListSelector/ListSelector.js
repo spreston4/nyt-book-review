b04f2e452f4e85bc65b9cf6408f898d2f407ad29
Sam Preston <sam.preston11@gmail.com>
2022-01-25 10:33:12 -0800

    ListSelector fetches complete list names from NYT API.

--- src/components/ListSelector/ListSelector.js
+++ src/components/ListSelector/ListSelector.js
@@ -1,8 +1,25 @@
-import {useState, useEffect} from 'react';
-import styles from './ListSelector.module.css';
+import { useState, useEffect } from "react";
+import styles from "./ListSelector.module.css";
 
 const ListSelector = (props) => {
-    useEffect(() => {}, []);
+  useEffect(() => {
+    const fetchLists = async () => {
+      const response = await fetch(
+        `https://api.nytimes.com/svc/books/v3/lists/names.json?&api-key=${process.env.REACT_APP_API_KEY}`
+      );
+
+      if (!response.ok) {
+        console.log("error fetching");
+        return;
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+    };
+
+    fetchLists();
+  }, []);
 
   return (
     <div className={styles.selector}>
