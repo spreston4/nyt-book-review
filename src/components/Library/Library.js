9c5cc8d367da0828c2935d8d4dcd7b929ea20582
Sam Preston <sam.preston11@gmail.com>
2022-01-26 10:10:54 -0800

    Added loading state & styling to library.

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
 import styles from "./Library.module.css";
 import BookItem from "../BookItem/BookItem";
+import loadingImage from "../../assets/images/Spinner-1s-200px.gif";
 
 // Fetches book results for the selected list. Displays a BookItem for each result to the user. Results filtered to 10.
 const Library = (props) => {
-    const [libraryArray, setLibraryArray] = useState([]);
+  const [libraryArray, setLibraryArray] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   // Fetch book results for selected list
   useEffect(() => {
     const fetchBooks = async (searchList) => {
+      setIsLoading(true);
       try {
         const response = await fetch(
           `https://api.nytimes.com/svc/books/v3/lists/current/${searchList}.json?api-key=${process.env.REACT_APP_API_KEY}`
@@ -16,29 +19,29 @@ const Library = (props) => {
 
         const responseData = await response.json();
         const libraryData = responseData.results.books;
-        
+
         // Populate array with relevant data
         const loadedBooks = [];
         for (const key in libraryData) {
-            loadedBooks.push({
-                id: key,
-                amazonUrl: libraryData[key].amazon_product_url,
-                author: libraryData[key].author,
-                bookImage: libraryData[key].book_image,
-                description: libraryData[key].description,
-                isbn13: libraryData[key].primary_isbn13,
-                publisher: libraryData[key].publisher,
-                rank: libraryData[key].rank,
-                rankLastWeek: libraryData[key].rank_last_week,
-                title: libraryData[key].title,
-                weeksOnList: libraryData[key].weeks_on_list,
-            });
+          loadedBooks.push({
+            id: key,
+            amazonUrl: libraryData[key].amazon_product_url,
+            author: libraryData[key].author,
+            bookImage: libraryData[key].book_image,
+            description: libraryData[key].description,
+            isbn13: libraryData[key].primary_isbn13,
+            publisher: libraryData[key].publisher,
+            rank: libraryData[key].rank,
+            rankLastWeek: libraryData[key].rank_last_week,
+            title: libraryData[key].title,
+            weeksOnList: libraryData[key].weeks_on_list,
+          });
         }
 
         setLibraryArray(loadedBooks);
-
+        setIsLoading(false);
       } catch (error) {
-        console.error('Error fetching books', error);
+        console.error("Error fetching books", error);
       }
     };
     fetchBooks(props.selectedList);
@@ -46,9 +49,9 @@ const Library = (props) => {
 
   return (
     <div className={styles.library}>
-      {libraryArray.map((book) => (
-          <BookItem book={book}/>
-      ))}
+      {isLoading && <img src={loadingImage} />}
+      {!isLoading &&
+        libraryArray.map((book) => <BookItem key={book.id} book={book} />)}
     </div>
   );
 };
