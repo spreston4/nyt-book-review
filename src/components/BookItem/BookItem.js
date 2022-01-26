58d9d3c8516464a93293b211a03f0683bce74b96
Sam Preston <sam.preston11@gmail.com>
2022-01-26 09:16:29 -0800

    Limits characters of title and description on Bookitem to 30 & 50.

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -1,13 +1,31 @@
 import styles from "./BookItem.module.css";
 
 const BookItem = (props) => {
+  // Limit title to 30 characters.
+  const titleHandler = (title) => {
+    if (title.length > 30) {
+      return title.slice(0, 29) + "...";
+    } else {
+      return title;
+    }
+  };
+
+  // Limit description on BookItem to 50 characters.
+  const descriptionHandler = (description) => {
+    if (description.length > 50) {
+      return description.slice(0, 49) + "...";
+    } else {
+      return description;
+    }
+  };
+
   return (
     <div className={styles.book}>
-        <div className={styles.content}>
-        <h3>{props.book.title}</h3>
-        <p>{props.book.description}</p>
+      <div className={styles.content}>
+        <h3>{titleHandler(props.book.title)}</h3>
+        <p>{descriptionHandler(props.book.description)}</p>
         <button>Book Details</button>
-        </div>
+      </div>
       <div className={styles.foreground}></div>
       <div className={styles.background}>
         <img src={props.book.bookImage} alt={`Cover for ${props.book.title}`} />
