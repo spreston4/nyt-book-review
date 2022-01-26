17da42636edc850c1402010dd6443bf7b42cab89
Sam Preston <sam.preston11@gmail.com>
2022-01-26 14:14:20 -0800

    BookModal now fetches review info for selected book.

--- src/components/BookModal/BookModal.js
+++ src/components/BookModal/BookModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createPortal } from "react-dom";
 import styles from "./BookModal.module.css";
 
@@ -9,7 +9,22 @@ const BackdropOverlay = () => {
 
 const BookModal = (props) => {
   const modalElement = document.getElementById("modal-root");
-  
+
+  useEffect(() => {
+    const fetchReviews = async (isbn) => {
+      try {
+        const response = await fetch(
+          `https://api.nytimes.com/svc/books/v3/reviews.json?isbn=${isbn}&api-key=${process.env.REACT_APP_API_KEY}`
+        );
+
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (error) {
+        console.error("Error fetching reviews", error);
+      }
+    };
+    fetchReviews(props.book.isbn13);
+  }, [props.book]);
 
   return createPortal(
     <React.Fragment>
