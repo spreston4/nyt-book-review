f9e12538f373422164ece594c69e203952663f2a
Sam Preston <sam.preston11@gmail.com>
2022-01-26 08:59:56 -0800

    BookItem styling.

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -3,13 +3,14 @@ import styles from "./BookItem.module.css";
 const BookItem = (props) => {
   return (
     <div className={styles.book}>
-      <div className={styles.foreground}>
+        <div className={styles.content}>
         <h3>{props.book.title}</h3>
         <p>{props.book.description}</p>
         <button>Book Details</button>
-      </div>
+        </div>
+      <div className={styles.foreground}></div>
       <div className={styles.background}>
-          <img src={props.book.bookImage} alt={`Cover for ${props.book.title}`}/>
+        <img src={props.book.bookImage} alt={`Cover for ${props.book.title}`} />
       </div>
     </div>
   );
